dce2ad6bf41cdc0043e21c91a2b307e4486c697d
Jakub Kozak <kozak.jakub55@gmail.com>
2020-07-09 20:56:09 +0200

    Move Projects to the landing page

--- my-app/src/components/landingpage.js
+++ my-app/src/components/landingpage.js
@@ -1,53 +1,79 @@
-import React, {Component} from 'react';
-import {Grid, Cell} from 'react-mdl';
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+
+import Projects from "./projects";
 
 class Landing extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          width: "100%",
+          margin: "auto",
+        }}
+      >
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://i.ya-webdesign.com/images/png-avatar-4.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <div className="banner-txt">
+              <h1>Full Stack Web Developer</h1>
 
-    render() {
-        return (<div style={{
-                width: '100%',
-                margin: 'auto'
-            }}>
-            <Grid className="landing-grid">
-                <Cell col={12}>
-                    <img src="https://i.ya-webdesign.com/images/png-avatar-4.png" alt="avatar" className="avatar-img"/>
-                    <div className="banner-txt">
-                        <h1>Full Stack Web Developer</h1>
+              <hr />
 
-                        <hr/>
+              <p>
+                HTML/CSS | Bootstrap | React | React Native | NodeJS | MongoDB |
+                Expres
+              </p>
+              <div className="social-links">
+                {/* LinkedIn */}
+                <a
+                  href="http://gooogle.com"
+                  rel="noopener referrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
 
-                        <p>HTML/CSS | Bootstrap | React | React Native | NodeJS | MongoDB | Expres
-                        </p>
-                        <div className="social-links">
+                {/* github */}
+                <a
+                  href="http://gooogle.com"
+                  rel="noopener referrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
 
-                            {/* LinkedIn */}
-                            <a href="http://gooogle.com" rel="noopener referrer" target="_blank">
-                                <i class="fa fa-linkedin-square" aria-hidden="true"/>
-                            </a>
+                {/* Freecodecamp */}
+                <a
+                  href="http://gooogle.com"
+                  rel="noopener referrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-free-code-camp" aria-hidden="true" />
+                </a>
 
-                            {/* github */}
-                            <a href="http://gooogle.com" rel="noopener referrer" target="_blank">
-                                <i class="fa fa-github-square" aria-hidden="true"/>
-                            </a>
-
-                            {/* Freecodecamp */}
-                            <a href="http://gooogle.com" rel="noopener referrer" target="_blank">
-                                <i class="fa fa-free-code-camp" aria-hidden="true"/>
-                            </a>
-
-                            {/* Youtube */}
-                            <a href="http://gooogle.com" rel="noopener referrer" target="_blank">
-                                <i class="fa fa-youtube-square" aria-hidden="true"/>
-                            </a>
-
-                        </div>
-                    </div>
-
-                </Cell>
-            </Grid>
-
-        </div>)
-    }
+                {/* Youtube */}
+                <a
+                  href="http://gooogle.com"
+                  rel="noopener referrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-youtube-square" aria-hidden="true" />
+                </a>
+              </div>
+            </div>
+          </Cell>
+          <Cell col={12}>
+            <Projects />
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Landing;
